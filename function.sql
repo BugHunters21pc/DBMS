2a91c6c9f378e41f253d278adccec859aec099f3
BugHunters21pc <116105892+BugHunters21pc@users.noreply.github.com>
2022-10-26 09:27:22 +0530

    Update function.sql

--- function.sql
+++ function.sql
@@ -21,3 +21,61 @@ BEGIN
 END;
 /
 
+DECLARE
+ c number(2);
+BEGIN
+ c := totalCustomers();
+ dbms_output.put_line('Total no. of Customers: ' || c);
+END;
+/
+
+EXAMPLE :
+
+DECLARE
+ a number;
+ b number;
+ c number;
+FUNCTION findMax(x IN number, y IN number)
+RETURN number
+IS
+ z number;
+BEGIN
+ IF x > y THEN
+ z:= x;
+ ELSE
+ Z:= y;
+ END IF;
+ RETURN z;
+END;
+
+BEGIN
+ a:= 23;
+ b:= 45;
+ c := findMax(a, b);
+ dbms_output.put_line(' Maximum of (23,45): ' || c);
+END;
+/
+
+EXAMPLE FACTORIAL:
+
+DECLARE
+ num number;
+ factorial number;
+FUNCTION fact(x number)
+RETURN number
+IS
+ f number;
+ BEGIN
+ IF x=0 THEN
+ f := 1;
+ ELSE
+ f := x * fact(x-1);
+ END IF;
+RETURN f;
+END;
+BEGIN
+ num:= 6;
+ factorial := fact(num);
+ dbms_output.put_line(' Factorial '|| num || ' is ' || factorial);
+END;
+/
