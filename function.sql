551c6553c569d849dcd7554cc91f0b04c1dbbb94
BugHunters21pc <116105892+BugHunters21pc@users.noreply.github.com>
2022-10-26 09:15:08 +0530

    Update function.sql

---
+++ function.sql
@@ -0,0 +1,23 @@
+SYNTAX:
+
+CREATE [OR REPLACE] FUNCTION function_name
+[(parameter_name [IN | OUT | IN OUT] type [, ...])]
+RETURN return_datatype
+{IS | AS}
+BEGIN
+ < function_body >
+END [function_name];
+
+EXAMPLE:
+
+CREATE OR REPLACE FUNCTION totalCustomers
+RETURN number IS
+ total number(2) := 0;
+BEGIN
+ SELECT count(*) into total
+ FROM customers;
+
+ RETURN total;
+END;
+/
+
